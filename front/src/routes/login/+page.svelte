9196205489614633afdc4a6e0eb2f8f98f779455
lakonguina <laury.akonguina@icloud.com>
2023-12-02 23:20:28 +0100

    clear front

---
+++ front/src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Login</title>
+	<meta name="description" content="Login" />
+</svelte:head>
+
+<form method="POST" action="/login">
+	<div>
+		<label for="name">Username</label>
+    	<input type="text" name="name" required>
+	</div>
+
+	<div>
+    	<label for="password">Password</label>
+    	<input type="password" name="password" required>
+	</div>
+
+	<button type="submit">Login</button>
+</form>
